e6de1fc4e87cc47a324fcffaae0b55572015b401
Seth Kirby <slkirby22@gmail.com>
2024-11-12 20:03:57 -0500

    Tues, Nov 12
    Basic Working Prototype. Finished Chapter 31.

--- SpaceShmup/Assets/__Scripts/Hero.cs
+++ SpaceShmup/Assets/__Scripts/Hero.cs
@@ -10,9 +10,14 @@ public class Hero : MonoBehaviour
     public float speed = 30;
     public float rollMult = -45;
     public float pitchMult = 30;
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 40;
 
-    [Header("Dynamic")] [Range(0,4)]
-    public float shieldLevel = 1;
+    [Header("Dynamic")]
+    [Range(0, 4)]
+    private float _shieldLevel = 1;
+    [Tooltip("This field holds a reference to the last triggering GameObject")]
+    private GameObject lastTriggerGo = null;
 
     void Awake()
     {
@@ -37,5 +42,52 @@ public class Hero : MonoBehaviour
         transform.position = pos;
 
         transform.rotation = Quaternion.Euler(vAxis*pitchMult, hAxis*rollMult, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TempFire();
+        }
+    }
+
+    void TempFire()
+    {
+        GameObject projGO = Instantiate<GameObject>(projectilePrefab);
+        projGO.transform.position = transform.position;
+        Rigidbody rigidB = projGO.GetComponent<Rigidbody>();
+        rigidB.velocity = Vector3.up * projectileSpeed;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go = rootT.gameObject;
+
+        if (go == lastTriggerGo) return;
+        lastTriggerGo = go;
+
+        Enemy enemy = go.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            shieldLevel--;
+            Destroy(go);
+        }
+        else
+        {
+            Debug.LogWarning("Shield trigger hit by non-Enemy: " + go.name);
+        }
+    }
+
+    public float shieldLevel
+    {
+        get { return (_shieldLevel); }
+        private set
+        {
+            _shieldLevel = Mathf.Min(value, 4);
+            if (value < 0)
+            {
+                Destroy(this.gameObject);
+                Main.HERO_DIED();
+            }
+        }
     }
 }
